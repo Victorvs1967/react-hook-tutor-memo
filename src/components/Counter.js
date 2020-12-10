f3c3a345fb47098f482bd09789b0c2c62368113d
Victor <victorsmirnov@me.com>
2020-12-10 13:33:45 +0200

    222

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 const Counter = () => {
 
@@ -7,11 +7,17 @@ const Counter = () => {
 
     const incrementOne = () => setCounterOne(counterOne + 1);
     const incrementTwo = () => setCounterTwo(counterTwo + 1);
+    const isEvent = useMemo(() => {
+        let i = 0;
+        while (i < 200000000) i++;
+        return counterOne % 2 === 0;
+    }, [counterOne]);
 
     return (
         <>
             <div>
                 <button onClick={incrementOne}>Count One - {counterOne}</button>
+                <span>{isEvent ? ' Event' : ' Odd'}</span>
             </div>
             <div>
                 <button onClick={incrementTwo}>Count Two - {counterTwo}</button>
